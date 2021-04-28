705e25e884bf3c0e7ab48d14dc9f30c893d280f8
PrasadBroo <PrasadBroo@users.noreply.github.com>
2021-04-28 23:41:01 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import HomePage from "./pages/HomePage";
 import "./css/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UsernamePage from "./pages/UsernamePage";
+import Test from "./pages/Test";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={() => <HomePage />}></Route>
-          <Route exact path="/:username" component={() => <UsernamePage />}></Route>
-          {/* <Route exact path="/login" component={()=> < Loginpage login={(email,pass)=>loginuser(email,pass)} user={user} />}></Route>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <HomePage />}></Route>
+        <Route
+          exact
+          path="/:username"
+          component={() => <UsernamePage />}
+        ></Route>
+        <Route exact path="/test" component={() => <Test />}></Route>
+        {/* <Route exact path="/login" component={()=> < Loginpage login={(email,pass)=>loginuser(email,pass)} user={user} />}></Route>
           <Route exact path="/signup" component={()=> < Signupage signup={(e,p,cp)=>signupuser(e,p,cp)} user={user}  />}></Route> */}
-        </Switch>
-      </Router>
-    </div>
+      </Switch>
+    </Router>
   );
 }
 
