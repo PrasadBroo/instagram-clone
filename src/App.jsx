01d406f96556f56721aa5d6fc796cf03051c8397
PrasadBroo <PrasadBroo@users.noreply.github.com>
2021-04-29 00:01:35 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HomePage from "./pages/HomePage";
 import "./css/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UsernamePage from "./pages/UsernamePage";
-import Test from "./pages/Test";
+
 
 function App() {
   return (
@@ -14,7 +14,6 @@ function App() {
           path="/:username"
           component={() => <UsernamePage />}
         ></Route>
-        <Route exact path="/test" component={() => <Test />}></Route>
         {/* <Route exact path="/login" component={()=> < Loginpage login={(email,pass)=>loginuser(email,pass)} user={user} />}></Route>
           <Route exact path="/signup" component={()=> < Signupage signup={(e,p,cp)=>signupuser(e,p,cp)} user={user}  />}></Route> */}
       </Switch>
---
+++ src/stores/store.js
@@ -0,0 +1,5 @@
+import { store } from "@risingstack/react-easy-state";
+
+const mystore = store([0]);
+
+export default mystore;
