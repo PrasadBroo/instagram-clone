92e0cc47710f1668d315a4427efdc1016960e322
PrasadBroo <PrasadBroo@users.noreply.github.com>
2021-04-30 19:30:25 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import "./css/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UsernamePage from "./pages/UsernamePage";
 import { useEffect } from "react";
-import {auth} from './services/firebase'
-import store from './stores/store'
+import { auth } from "./services/firebase";
+import store from "./stores/store";
 import { view } from "@risingstack/react-easy-state";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  useEffect(()=>{
-    auth().onAuthStateChanged(user=>  store.auth.user = user );
-},[])
+  useEffect(() => {
+    auth().onAuthStateChanged((user) => (store.auth.user = user));
+  }, []);
   return (
     <Router>
       <Switch>
@@ -22,9 +22,16 @@ function App() {
           path="/:username"
           component={() => <UsernamePage />}
         ></Route>
-        <Route exact path="/accounts/emailsignup" component={() => <SignupPage />}></Route>
-        <Route exact path="/accounts/login" component={() => < LoginPage/>}></Route>
-        
+        <Route
+          exact
+          path="/accounts/emailsignup"
+          component={() => <SignupPage />}
+        ></Route>
+        <Route
+          exact
+          path="/accounts/login"
+          component={() => <LoginPage />}
+        ></Route>
       </Switch>
     </Router>
   );
